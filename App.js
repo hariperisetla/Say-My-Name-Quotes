5f8f6f4c63c12e83b5c9db9e080bc80762cd6d1d
Hari Perisetla <harinagasai.perisetla@gmail.com>
2023-03-13 12:31:52 +0530

    ui changed

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import {
   FlatList,
   RefreshControl,
   SafeAreaView,
+  TouchableOpacity,
+  ActivityIndicator,
 } from "react-native";
 
 import { ThemeProvider, Button, createTheme } from "@rneui/themed";
@@ -14,6 +16,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -66,61 +69,109 @@ function HomeScreen({ navigation }) {
     }
   };
 
+  const onPress = () => {
+    fetchQuotes();
+  };
+
   useEffect(() => {
     fetchQuotes();
   }, []);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView
+      style={{ backgroundColor: "#032202", flex: 1, justifyContent: "center" }}
+    >
       <View
         style={{
-          padding: 20,
-          justifyContent: "center",
-          alignItems: "center",
+          // padding: 20,
+          // justifyContent: "center",
+          // alignItems: "center",
+          margin: 2,
+          padding: 0,
           // flex: 1,
         }}
       >
         {loading ? (
-          <Text>Loading...</Text>
+          <ActivityIndicator size="large" color="#369457" />
         ) : (
           <FlatList
             numColumns={1}
             keyExtractor={({ index }) => index + 1}
             data={quote}
-            contentContainerStyle={{ flexGrow: 1, justifyContent: "center" }}
+            contentContainerStyle={
+              {
+                // flexGrow: 1,
+                // justifyContent: "center",
+                // backgroundColor: "",
+              }
+            }
             refreshControl={
               <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
             }
             renderItem={({ item }) => (
-              <View
-                style={{
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Text
+              <View>
+                <View
                   style={{
-                    // fontFamily: "Poppins_400Regular",
-                    fontFamily: "Poppins_600SemiBold",
-                    fontSize: 30,
-                    // fontWeight: 700,
-                    textTransform: "capitalize",
-                    lineHeight: 55,
+                    // alignItems: "center",
+                    justifyContent: "center",
+                    backgroundColor: "#369457",
+                    padding: 30,
+                    margin: 3,
+                    // borderWidth: 2,
+                    borderRadius: 50,
                   }}
                 >
-                  {item.quote}
-                </Text>
-                <Text
-                  style={{
-                    paddingTop: 10,
-                    fontSize: 20,
-                    textAlign: "right",
-                    fontFamily: "Poppins_500Medium_Italic",
-                    color: "gray",
-                  }}
-                >
-                  - {item.author}
-                </Text>
+                  <Text style={{ textAlign: "left" }}>
+                    <FontAwesome name="quote-left" size={50} />
+                  </Text>
+                  <Text
+                    style={{
+                      // fontFamily: "Poppins_400Regular",
+                      fontFamily: "Poppins_600SemiBold",
+                      fontSize: 20,
+                      // fontSizeAdjust: "inherit",
+                      // fontWeight: 700,
+                      textTransform: "capitalize",
+                      lineHeight: 55,
+                    }}
+                  >
+                    {item.quote}
+                  </Text>
+                  <Text
+                    style={{
+                      paddingTop: 10,
+                      fontSize: 20,
+                      textAlign: "right",
+                      fontFamily: "Poppins_500Medium_Italic",
+                      color: "#0D3E10",
+                    }}
+                  >
+                    - {item.author}
+                  </Text>
+                </View>
+                {/* <View>
+                  <TouchableOpacity
+                    onPress={onPress}
+                    style={{
+                      borderRadius: 50,
+                      backgroundColor: "#45BD6F",
+                      paddingHorizontal: 10,
+                      paddingVertical: 15,
+                      marginHorizontal: 3,
+                      marginVertical: 8,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        fontSize: 20,
+                        fontFamily: "Poppins_700Bold",
+                        textAlign: "center",
+                      }}
+                    >
+                      Random Quote
+                    </Text>
+                  </TouchableOpacity>
+                </View> */}
               </View>
             )}
           />
@@ -131,6 +182,29 @@ function HomeScreen({ navigation }) {
           //   </View>
           // ))
         )}
+      </View>
+      <View>
+        <TouchableOpacity
+          onPress={onPress}
+          style={{
+            borderRadius: 50,
+            backgroundColor: "#45BD6F",
+            paddingHorizontal: 10,
+            paddingVertical: 15,
+            marginHorizontal: 3,
+            marginVertical: 8,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 20,
+              fontFamily: "Poppins_700Bold",
+              textAlign: "center",
+            }}
+          >
+            Random Quote
+          </Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
@@ -157,10 +231,28 @@ const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        // headerShown: false,
+        tabBarActiveTintColor: "#369457",
+        tabBarInactiveTintColor: "#1F6032",
+        tabBarStyle: {
+          backgroundColor: "#032202",
+          // paddingTop: 0,
+          // borderTopWidth: 0,
+        },
+      })}
+    >
       <Tab.Screen
-        name="Home"
+        name="Say My Name"
         options={{
+          headerStyle: {
+            backgroundColor: "#032202",
+          },
+          headerTitleStyle: {
+            fontFamily: "Poppins_600SemiBold",
+          },
+          headerTintColor: "#369457",
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="home" color={color} size={size} />
           ),
@@ -214,10 +306,12 @@ export default function App() {
     SplashScreen.hideAsync();
   }
   return (
-    <NavigationContainer>
-      {/* <Text style={{ fontFamily: "Poppins_400Regular" }}>Home Font</Text> */}
-      <TabNavigation />
-    </NavigationContainer>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+      <NavigationContainer>
+        {/* <Text style={{ fontFamily: "Poppins_400Regular" }}>Home Font</Text> */}
+        <TabNavigation />
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 
